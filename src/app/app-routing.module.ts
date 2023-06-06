ed2278a3c1ec9dd6c8d7a62edc7597deebf04930
Adrian Lazarec <lazarec.adrian@gmail.com>
2023-06-06 15:18:56 +0300

    created devices component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersListComponent } from './components/users/users-list/users-list.component';
+import { DevicesListComponent } from './components/devices/devices-list/devices-list.component';
 
 const routes: Routes = [
   {
     path: '',
     component: UsersListComponent
+  },
+
+  {
+    path: '/devices',
+    component: DevicesListComponent
   }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersListComponent } from './components/users/users-list/users-list.component';
+import { DevicesListComponent } from './components/devices/devices-list/devices-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersListComponent
+    UsersListComponent,
+    DevicesListComponent
   ],
   imports: [
     BrowserModule,
