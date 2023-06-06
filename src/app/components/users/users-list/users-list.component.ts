35f6448140b8186a7331df24122889fc3763bf9e
Adrian Lazarec <lazarec.adrian@gmail.com>
2023-06-06 16:12:15 +0300

    created users table with dummy data

--- src/app/components/users/users-list/users-list.component.ts
+++ src/app/components/users/users-list/users-list.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.css']
 })
-export class UsersListComponent {
+export class UsersListComponent  {
+
+  users: User[] = [
+    {
+      userID: 1,
+      name: 'Adrian Lazarec',
+      role: 'Support Associate',
+      location: 'Cluj-Napoca'
+    },
+    {
+      userID: 2,
+      name: 'dghasdehyghas',
+      role: 'Support Associate',
+      location: 'Cluj-Napoca'
+    },
+    {
+      userID: 3,
+      name: 'agsdsadgasgdagdasg',
+      role: 'Support Associate',
+      location: 'Cluj-Napoca'
+    }
+  ];
+
+  
 
 }
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,7 @@
+export interface User
+{
+    userID: number;
+    name: string;
+    role: string;
+    location: string;
+}
