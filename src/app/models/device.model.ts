254f824d4da4ce6dba232588df913ae4c3835023
Adrian Lazarec <lazarec.adrian@gmail.com>
2023-06-07 15:55:00 +0300

    Added device model

---
+++ src/app/models/device.model.ts
@@ -0,0 +1,12 @@
+export interface Device
+{
+  DeviceID: number;
+  UserID: number;
+  Name: string;
+  Manufacturer: string;
+  Type: string;
+  OperatingSystem: string;
+  OSVersion: string;
+  Processor: string;
+  RAMAmount: string;
+}
